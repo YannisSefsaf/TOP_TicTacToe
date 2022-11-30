36ae02eaee4590cc9d895318756c1a6a88daab72
YannisSefsaf <111019910+YannisSefsaf@users.noreply.github.com>
2022-11-30 16:20:28 -0600

    Reworked some of the logic and tucked everything inside modules & factory functions

--- script.js
+++ script.js
@@ -1,162 +1,223 @@
-const cells = document.querySelectorAll(".cell");
-const grid = document.querySelector(".grid");
-const form = document.querySelector(".form");
-const formIntro = document.querySelector(".form__intro");
-const reset = document.querySelector(".reset");
-const para = document.querySelector(".para");
-const overlay = document.querySelector(".overlay");
-const btnPlayGame = document.querySelector(".startGame");
-const userOne = document.querySelector("#username1");
-const userTwo = document.querySelector("#username2");
-const myPlayer = userOne.value;
-const myComputer = userTwo.value;
-const userNameOne = document.querySelector(".player--one");
-const userNameTwo = document.querySelector(".player--two");
-const userNameOneSign = document.querySelector(".player--one--sign");
-const userNameTwoSign = document.querySelector(".player--two--sign");
-const outcome = document.querySelector(".outcome");
-
-btnPlayGame.addEventListener("click", (e) => {
-  e.preventDefault();
-  let JoueurUn = Player(userOne.value, "x");
-  let JoueurDeux = Player(userTwo.value, "o");
-  formIntro.classList.add("hidden");
-  overlay.classList.add("hidden");
-  grid.classList.remove("hidden");
-  userNameOne.textContent = userOne.value;
-  userNameTwo.textContent = userTwo.value;
-  userNameOneSign.textContent = JoueurUn.getSign();
-  userNameTwoSign.textContent = JoueurDeux.getSign();
-  const playGame = game(JoueurUn.getPlayer(), JoueurDeux.getPlayer());
-  playGame.play();
-});
-
-const resetGame = () => {
-  cells.forEach((cell) => (cell.innerHTML = ""));
-  let JoueurUn = Player(userOne.value, "x");
-  let JoueurDeux = Player(userTwo.value, "o");
-  form.classList.add("hidden");
-  overlay.classList.add("hidden");
-  userNameOne.textContent = userOne.value;
-  userNameTwo.textContent = userTwo.value;
-  userNameOneSign.textContent = JoueurUn.getSign();
-  userNameTwoSign.textContent = JoueurDeux.getSign();
-  const playGame = game(JoueurUn.getPlayer(), JoueurDeux.getPlayer());
-  let gameboard = gameBoard.resetGameBoard();
-  playGame.play();
-};
-
-reset.addEventListener("click", resetGame);
-
-// GAMEBOARD OBJECT // MODULE
-
-const gameBoard = (() => {
-  let gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const getGameBoard = () => gameboard;
-  const resetGameBoard = () => (gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9]);
-  return { getGameBoard, resetGameBoard };
-})();
-
-// PLAYER OBJECT // Factory Function
-
+// const Player
 const Player = (player, sign) => {
   const getPlayer = () => player;
   const getSign = () => sign;
   return { getPlayer, getSign };
 };
 
-// GAME OBJECT
+// const gameBoard
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
 
-const game = (player, computer, sign) => {
-  const playerOne = Player(userOne.value, "x");
-  const playerTwo = Player(userTwo.value, "o");
-  const { getPlayer, getSign } = Player(player, sign);
-  outcome.textContent = `It's ${playerOne.getPlayer()}'s turn!`;
-  const play = () => {
-    playRound();
+  const setCell = (index, sign) => {
+    board[index] = sign;
   };
-  const playRound = () => {
-    cells.forEach((cell) =>
-      cell.addEventListener(
-        "click",
-        (e) => {
-          let gameboard = gameBoard.getGameBoard();
-          let cellNum = e.target.getAttribute("data-attribute");
-          const countX = {};
-          const countO = {};
-          gameboard.forEach((element) => {
-            countX[element] = (countX[element] || 0) + 1;
-          });
-          gameboard.forEach((element) => {
-            countO[element] = (countO[element] || 0) + 1;
-          });
-          if (
-            !(gameboard[cellNum - 1] === "x") &&
-            !(gameboard[cellNum - 1] === "o")
-          ) {
-            if (
-              countX[playerOne.getSign()] > countO[playerTwo.getSign()] ||
-              (countX[playerOne.getSign()] === 1 &&
-                countO[playerTwo.getSign()] === undefined)
-            ) {
-              outcome.textContent = `It's ${playerOne.getPlayer()}'s turn!`;
-              gameboard[cellNum - 1] = `${playerTwo.getSign()}`;
-              gameboard.forEach((element) => {
-                countO[element] = (countO[element] || 0) + 1;
-              });
-              cells[cellNum - 1].innerHTML = gameboard[cellNum - 1];
-            } else {
-              outcome.textContent = `It's ${playerTwo.getPlayer()}'s turn!`;
-              gameboard[cellNum - 1] = `${playerOne.getSign()}`;
-              gameboard.forEach((element) => {
-                countX[element] = (countX[element] || 0) + 1;
-              });
-              cells[cellNum - 1].innerHTML = gameboard[cellNum - 1];
-            }
 
-            if (countO.o + countX.x === 13) {
-              form.classList.remove("hidden");
-              overlay.classList.remove("hidden");
-              para.innerHTML = "Draw!";
-            }
-            if (winningCombos()) {
-              outcome.textContent = "";
-              if (countX[playerOne.getSign()] > countO[playerTwo.getSign()]) {
-                para.innerHTML = `${playerOne.getPlayer()} won!`;
-              } else {
-                para.innerHTML = `${playerTwo.getPlayer()} won!`;
-              }
-              form.classList.remove("hidden");
-              overlay.classList.remove("hidden");
-            }
-          }
-          return true;
-        },
-        { once: true }
-      )
+  const getCell = (index) => board[index];
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = "";
+    }
+  };
+
+  return { setCell, getCell, reset };
+})();
+
+// const displayController
+const displayController = (() => {
+  // grid and cells //
+  const grid = document.querySelector(".grid");
+  const cells = document.querySelectorAll(".cell");
+  const turn = document.querySelector(".turn");
+
+  // player and result forms & overlay //
+  const formIntro = document.querySelector(".form__intro");
+  const formResult = document.querySelector(".form__result");
+  const result = document.querySelector(".result");
+  const overlay = document.querySelector(".overlay");
+  const resetButton = document.querySelector(".resetBtn");
+  const userOne = document.querySelector("#username1");
+  const userTwo = document.querySelector("#username2");
+  let userNameOne;
+  let userNameTwo;
+
+  // start button
+  const playGameButton = document.querySelector(".startGame");
+
+  // BUTTON EVENT LISTENERS //
+
+  playGameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    setPlayers();
+    gameController.updatePlayers();
+    showGameBoard();
+    activateCells();
+    setTurnMessage(`It's ${getPlayerOne()}'s turn`);
+  });
+
+  resetButton.addEventListener("click", () => {
+    showGameBoardAfterReset();
+    gameBoard.reset();
+    gameController.reset();
+    updateGameboard();
+    setTurnMessage(`It's ${getPlayerOne()}'s turn`);
+  });
+
+  // BOARD LOGIC //
+
+  const activateCells = () => {
+    cells.forEach((cell) =>
+      cell.addEventListener("click", (e) => {
+        if (!gameController.getIsOver() && e.target.textContent === "") {
+          gameController.playRound(
+            parseInt(e.target.getAttribute("data-attribute") - 1)
+          );
+          updateGameboard();
+        }
+      })
     );
   };
-  return {
-    getPlayer,
-    play,
-    playRound,
-    player,
-    computer,
-  };
-};
 
-const winningCombos = () => {
-  let gameboard = gameBoard.getGameBoard();
-  if (
-    (gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2]) ||
-    (gameboard[3] === gameboard[4] && gameboard[4] === gameboard[5]) ||
-    (gameboard[6] === gameboard[7] && gameboard[7] === gameboard[8]) ||
-    (gameboard[0] === gameboard[3] && gameboard[3] === gameboard[6]) ||
-    (gameboard[1] === gameboard[4] && gameboard[4] === gameboard[7]) ||
-    (gameboard[2] === gameboard[5] && gameboard[5] === gameboard[8]) ||
-    (gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8]) ||
-    (gameboard[2] === gameboard[4] && gameboard[4] === gameboard[6])
-  ) {
-    return true;
-  }
-};
+  const updateGameboard = () => {
+    for (let i = 0; i < cells.length; i++) {
+      cells[i].textContent = gameBoard.getCell(i);
+    }
+  };
+
+  // Players //
+
+  const setPlayers = () => {
+    userNameOne = userOne.value;
+    userNameTwo = userTwo.value;
+  };
+
+  const getPlayerOne = () => {
+    return userNameOne;
+  };
+
+  const getPlayerTwo = () => {
+    return userNameTwo;
+  };
+
+  // SHOW & HIDE BOARD AND FORMS //
+
+  const showGameBoard = () => {
+    formIntro.classList.add("hidden");
+    overlay.classList.add("hidden");
+    grid.classList.remove("hidden");
+  };
+
+  const showGameBoardAfterReset = () => {
+    turn.classList.remove("hidden");
+    formResult.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  const hideGameBoard = () => {
+    turn.classList.add("hidden");
+    formResult.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+  // TURNS AND RESULTS FORMS AND MESSAGES
+
+  const showResultForm = (winner) => {
+    hideGameBoard();
+    if (winner === "Draw") {
+      setResultMessage("It's a draw!");
+    } else {
+      setResultMessage(`${winner} has won!`);
+    }
+  };
+
+  const setResultMessage = (message) => {
+    result.textContent = message;
+  };
+
+  const setTurnMessage = (message) => {
+    turn.textContent = message;
+  };
+
+  return {
+    showResultForm,
+    setResultMessage,
+    setTurnMessage,
+    getPlayerOne,
+    getPlayerTwo,
+  };
+})();
+
+// const gameController
+
+const gameController = (() => {
+  // get players from form //
+  let playerOne = displayController.getPlayerOne();
+  let playerTwo = displayController.getPlayerTwo();
+  let playerX = Player(playerOne, "X");
+  let playerO = Player(playerTwo, "O");
+
+  const updatePlayers = () => {
+    playerOne = displayController.getPlayerOne();
+    playerTwo = displayController.getPlayerTwo();
+  };
+
+  let round = 1;
+  let isOver = false;
+
+  const playRound = (index) => {
+    playerX = Player(playerOne, "X");
+    playerO = Player(playerTwo, "O");
+    gameBoard.setCell(index, getCurrentPlayerSign());
+    if (checkWinner(index)) {
+      displayController.showResultForm(getCurrentPlayerName());
+      isOver = true;
+    }
+    if (!checkWinner(index) && round === 9) {
+      displayController.showResultForm("Draw");
+      isOver = true;
+    }
+    round++;
+    displayController.setTurnMessage(`It's ${getCurrentPlayerName()}'s turn`);
+  };
+
+  const getCurrentPlayerSign = () => {
+    return round % 2 === 1 ? playerX.getSign() : playerO.getSign();
+  };
+
+  const getCurrentPlayerName = () => {
+    return round % 2 === 1 ? playerX.getPlayer() : playerO.getPlayer();
+  };
+
+  const checkWinner = (index) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    return winConditions
+      .filter((combination) => combination.includes(index))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameBoard.getCell(index) === getCurrentPlayerSign()
+        )
+      );
+  };
+
+  const getIsOver = () => {
+    return isOver;
+  };
+
+  const reset = () => {
+    round = 1;
+    isOver = false;
+  };
+
+  return { playRound, getIsOver, reset, updatePlayers };
+})();
